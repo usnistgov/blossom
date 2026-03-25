f6b613a8c747d835e128ed2012b95d88def05a35
selenaxiao-nist <142448406+selenaxiao-nist@users.noreply.github.com>
2026-03-25 16:44:28 -0400

    nav base url

--- assets/js/load-nav.js
+++ assets/js/load-nav.js
@@ -1,8 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
-    fetch("/nav.html")
+    const isLocal = window.location.hostname === '127.0.0.1' || window.location.hostname === 'localhost';
+    const repoName = 'blossom';
+
+    const BASE_URL = isLocal ? '/' : `/${repoName}/`;
+
+    fetch(`${BASE_URL}nav.html`)
         .then(response => response.text())
         .then(data => {
-            document.getElementById("nav-main").innerHTML = data;
+             const fixedHTML = data.replace(/\{\{BASE_URL\}\}/g, BASE_URL);
+
+            document.getElementById("nav-main").innerHTML = fixedHTML;
         })
         .catch(error => console.error("Error loading the navigation: ", error));
 });
